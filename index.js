fe87c732827be3f511bc0499d14e5cc517e6546c
Rafaela Gomes <rafagiza@hotmail.com>
2024-05-27 11:14:59 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,25 @@
+function calcularRank(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem um saldo de ${saldoVitorias} está no nível de ${nivel}`);
+}
+
+// Exemplo de uso:
+calcularRank(70, 30);
